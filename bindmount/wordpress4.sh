ab9b8449585b8f2c868866b4beda7868b69226d2
Dominik Zogg <dominik.zogg@gmail.com>
2015-12-17 20:19:06 +0100

    define bind mount for wordpress4 cache

--- bindmount/wordpress4.sh
+++ bindmount/wordpress4.sh
@@ -1,1 +1,6 @@
-# not yet available
+echo "Executing bindmount for $1 ..."
+mkdir -p /tmp/$1/cache/
+chown -Rf vagrant:vagrant /tmp/$1/cache/
+mkdir -p /vagrant/wp-content/cache/
+mount -o bind /tmp/$1/cache/ /vagrant/wp-content/cache/
+echo "... done!"
