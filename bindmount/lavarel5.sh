3ae9e5e90f1bcba3057cd1c42a3ddd0686c38789
Dominik Zogg <dominik.zogg@gmail.com>
2015-12-17 20:16:05 +0100

    define bid mount for lavarel5 cache

--- bindmount/lavarel5.sh
+++ bindmount/lavarel5.sh
@@ -1,1 +1,6 @@
-# not yet available
+echo "Executing bindmount for $1 ..."
+mkdir -p /tmp/$1/cache/
+chown -Rf vagrant:vagrant /tmp/$1/cache/
+mkdir -p /vagrant/bootstrap/cache/
+mount -o bind /tmp/$1/cache/ /vagrant/bootstrap/cache/
+echo "... done!"
