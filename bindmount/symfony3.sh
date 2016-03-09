49dfeedfaeea1743731de9ff78ab4b43bd4e0d57
Dominik Zogg <dominik.zogg@gmail.com>
2016-03-09 19:37:45 +0100

    fix bind mounts on sf3

--- bindmount/symfony3.sh
+++ bindmount/symfony3.sh
@@ -1,10 +1,6 @@
 echo "Executing bindmount for $1, $2, $3 ..."
-mkdir -p $2/cache/
-mkdir -p $2/logs/
-chown -Rf vagrant:vagrant $2/cache/
-chown -Rf vagrant:vagrant $2/logs/
-mkdir -p $3/app/cache/
-mkdir -p $3/app/logs/
-mount -o bind $2/cache/ $3/app/cache/
-mount -o bind $2/logs/ $3/app/logs/
+mkdir -p $2/
+chown -Rf vagrant:vagrant $2/
+mkdir -p $3/var/
+mount -o bind $2 $3/var/
 echo "... done!"
