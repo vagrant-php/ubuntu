32d053c8d6cb8f85350d1ec980f844bf0bb06b93
Dominik Zogg <dominik.zogg@gmail.com>
2016-08-13 14:20:49 +0200

    add missing bind mount

---
+++ bindmount/bolt3.sh
@@ -0,0 +1,6 @@
+echo "Executing bindmount for $1, $2, $3 ..."
+mkdir -p $2/cache/
+chown -Rf vagrant:vagrant $2/cache/
+mkdir -p $3/app/cache/
+mount -o bind $2/cache/ $3/app/cache/
+echo "... done!"
