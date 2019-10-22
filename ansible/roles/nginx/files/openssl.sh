2faed8866e76ad7613b05d213daa88cc755c06e5
Samuel Breu <samuel.breu@artack.ch>
2019-10-22 15:46:50 +0200

    fixed generate ssl certs only if not in certs folder, respected mkcert generated certificates

--- ansible/roles/nginx/files/openssl.sh
+++ ansible/roles/nginx/files/openssl.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-# Call this script with at least 1 parameters, for example
-# ./openssl.sh HOSTNAME [SUBHOSTNAME]
+# ./openssl.sh HOSTNAME
 
-MIN_PARAMS=1
-IMPORT_CERT_DIR=/usr/local/share/ca-certificates/self-signed
+IMPORT_CERT_DIR=/vagrant/vagrant-php/certs-import
 EXPORT_CERT_DIR=/vagrant/vagrant-php/certs
+INNER_CERT_DIR=/usr/local/share/ca-certificates/self-signed
+NGINX_DIR=/etc/nginx
 
-if [[ $# -lt "$MIN_PARAMS" ]]
+if [[ ! $# -eq 1 ]]
 then
   echo
-  echo "This script needs at least $MIN_PARAMS command-line arguments!"
-  exit 0
+  echo "This script needs 1 command-line argument!"
+  exit 1
 fi
 
-if [[ ! -d "$IMPORT_CERT_DIR" ]]
+if [[ ! -d ${INNER_CERT_DIR} ]]
 then
-  mkdir -p ${IMPORT_CERT_DIR};
+    mkdir -p ${INNER_CERT_DIR};
 fi
 
-if [[ ! -d "$EXPORT_CERT_DIR" ]]
+if [[ ! -d ${EXPORT_CERT_DIR} ]]
 then
-  mkdir -p ${EXPORT_CERT_DIR};
+    mkdir -p ${EXPORT_CERT_DIR};
 fi
 
-if [[ $# -eq 1 ]]
+if [[ ! -f ${EXPORT_CERT_DIR}/$1.crt ]];
 then
-  echo
-  openssl req -nodes -x509 -newkey rsa:2048 -keyout /etc/nginx/$1.key -reqexts SAN -extensions SAN -out /etc/nginx/$1.crt -days 3650 -subj "/C=CH/ST=Zurich/L=Zurich/CN=$1" -config <(cat /etc/ssl/openssl.cnf; printf "[SAN]\nsubjectAltName=DNS:$1")
-  cp -f /etc/nginx/$1.crt ${EXPORT_CERT_DIR}/.
-  cp -f /etc/nginx/$1.crt ${IMPORT_CERT_DIR}/.
-  update-ca-certificates
-  exit 0
+    echo
+    openssl req -nodes -x509 -newkey rsa:2048 -sha256 -keyout /etc/nginx/$1.key -reqexts SAN -extensions SAN -out /etc/nginx/$1.crt -days 825 -subj "/C=CH/ST=Zurich/L=Zurich/CN=$1" -config <(cat /etc/ssl/openssl.cnf; printf "[SAN]\nsubjectAltName=DNS:$1")
+    cp -f /etc/nginx/$1.crt ${EXPORT_CERT_DIR}/.
+    cp -f /etc/nginx/$1.crt ${INNER_CERT_DIR}/.
 fi
+
+update-ca-certificates
+exit 0
