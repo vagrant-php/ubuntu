1b520e3b64ae94535544ff6a26798c9be6c49275
Samuel Breu <samuel.breu@artack.ch>
2020-05-02 23:15:18 +0200

    fixed ssl cert issues on vagrant destroy

--- ansible/roles/nginx/files/openssl.sh
+++ ansible/roles/nginx/files/openssl.sh
@@ -2,7 +2,7 @@
 
 # ./openssl.sh HOSTNAME
 
-EXPORT_CERT_DIR=/vagrant/vagrant-php/certs
+EXTERN_CERT_DIR=/vagrant/vagrant-php/certs
 INNER_CERT_DIR=/usr/local/share/ca-certificates/self-signed
 NGINX_DIR=/etc/nginx
 
@@ -18,18 +18,13 @@ then
     mkdir -p ${INNER_CERT_DIR};
 fi
 
-if [[ ! -d ${EXPORT_CERT_DIR} ]]
+if [[ ! -f ${EXTERN_CERT_DIR}/$1.crt || ! -f ${EXTERN_CERT_DIR}/$1.key ]];
 then
-    mkdir -p ${EXPORT_CERT_DIR};
+    openssl req -nodes -x509 -newkey rsa:2048 -sha256 -keyout ${EXTERN_CERT_DIR}/$1.key -reqexts SAN -extensions SAN -out ${EXTERN_CERT_DIR}/$1.crt -days 1024 -subj "/C=CH/ST=Zurich/L=Zurich/CN=$1" -config <(cat /etc/ssl/openssl.cnf; printf "[SAN]\nsubjectAltName=DNS:$1")
 fi
 
-if [[ ! -f ${EXPORT_CERT_DIR}/$1.crt ]];
-then
-    openssl req -nodes -x509 -newkey rsa:2048 -sha256 -keyout /etc/nginx/$1.key -reqexts SAN -extensions SAN -out /etc/nginx/$1.crt -days 825 -subj "/C=CH/ST=Zurich/L=Zurich/CN=$1" -config <(cat /etc/ssl/openssl.cnf; printf "[SAN]\nsubjectAltName=DNS:$1")
-    cp -f /etc/nginx/$1.crt ${EXPORT_CERT_DIR}/.
-    cp -f /etc/nginx/$1.crt ${INNER_CERT_DIR}/.
-    cp -f /etc/nginx/$1.crt ${NGINX_DIR}/.
-fi
+cp -f ${EXTERN_CERT_DIR}/$1.{key,crt} ${NGINX_DIR}/.
+cp -f ${EXTERN_CERT_DIR}/$1.crt ${INNER_CERT_DIR}/.
 
 update-ca-certificates
 exit 0
